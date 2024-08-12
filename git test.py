b1f611eeeccc343eea66fd23258a073d17f108f8
sardorSSS <sardorSSS@github.com>
2024-08-12 15:42:06 +0500

    123

---
+++ git test.py
@@ -0,0 +1,7 @@
+"""git init -в риеэстре только папка(запускает git )
+git add <name> or .
+git add . регистрирует всё внутри папки
+git commit -m <заметка>
+git push <хранилище> <ветка>
+get push origin main (main это название ветки)
+
